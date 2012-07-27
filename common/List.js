987b722c4b405ef467574f49b57b7fcd74a6deee
Miha-ha <miha-ha@yandex.ru>
2012-07-27 22:32:03 +0400

    Реорганизация кода - вынес все общие классы в common

--- common/List.js
+++ common/List.js
@@ -1,29 +1,29 @@
 Mix.define('List', {
-    init:function () {
+    init: function (){
         this.length = 0;
         this.items = {};
     },
-    add:function (index, item) {
+    add: function (index, item){
         this.items[index] = item;
         this.length++;
     },
-    remove:function (index) {
+    remove: function (index){
         delete this.items[index];
         this.length--;
     },
-    clear:function () {
+    clear: function (){
         for (var i in this.items) {
             delete this.items[i];
         }
         this.length = 0;
     },
-    get:function (index) {
+    get: function (index){
         if (index in this.items) {
             return this.items[index];
         }
         return null;
     },
-    each:function (f) {
+    each: function (f){
         for (var i in this.items) {
             if (false === f.call(this.items[i], i))
                 break;
